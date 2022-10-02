e51fe966d269572dc8369af44cf2f3bf36058a9f
Андрей <gosudarev_a@mail.ru>
2022-10-02 09:00:40 +0300

    Initial commit

---
+++ src/main/java/jd5/ShelterBot/shelterBot/ShelterBotApplication.java
@@ -0,0 +1,13 @@
+package jd5.ShelterBot.shelterBot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShelterBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShelterBotApplication.class, args);
+	}
+
+}
