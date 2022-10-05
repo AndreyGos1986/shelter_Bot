8dc73f7ad880c6ce4871ca680d56faa507b834d9
Verlonar <garvlok@gmail.com>
2022-10-05 17:18:52 +0300

    Созданы классы TelegramBotConfiguration, ShelterController, ParentUser, ShelterUser, репозитории под модели и сервис. Сами таблицы не добавлял, надо сначала опеределиться как будут выглядить по итогу модели юзеров, оставил в классах свои мысли по этому поводу. Бот уже работает, ему можно писать и он отвечает приветсвием с ником в телеге и заглушкой

---
+++ src/main/java/jd5/ShelterBot/shelterBot/model/ParentUser.java
@@ -0,0 +1,120 @@
+package jd5.ShelterBot.shelterBot.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+//Тут я бы убрал parentId и вместо него добавил бы ссылку на id в таблице пользователей.
+//Еще надо где-то в базе хранить фотки из отчета, можно создать еще одну талбицу,
+//в которой будут id, parentId и массив с фотками,а сюда добавить ссылку на id из таблицы с фотками
+
+
+@Entity
+public class ParentUser {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private Long parentId;
+    private String name;
+    private String surname;
+    private String phoneNumber;
+    private LocalDate lastReportDate;
+    private LocalDateTime lastNoticeDateAndTime;
+    private LocalDate endOfProbation;
+
+
+    public ParentUser(Long parentId, String name, String surname, String phoneNumber) {
+        this.parentId = parentId;
+        this.name = name;
+        this.surname = surname;
+        this.phoneNumber = phoneNumber;
+//        Определиться сколько будет длиться испытательный срок
+//        endOfProbation = LocalDate.now() +
+    }
+
+    public ParentUser() {
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getParentId() {
+        return parentId;
+    }
+
+    public void setParentId(Long parentId) {
+        this.parentId = parentId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public LocalDate getLastReportDate() {
+        return lastReportDate;
+    }
+
+    public void setLastReportDate(LocalDate lastReportDate) {
+        this.lastReportDate = lastReportDate;
+    }
+
+    public LocalDateTime getLastNoticeDateAndTime() {
+        return lastNoticeDateAndTime;
+    }
+
+    public void setLastNoticeDateAndTime(LocalDateTime lastNoticeDateAndTime) {
+        this.lastNoticeDateAndTime = lastNoticeDateAndTime;
+    }
+
+    public LocalDate getEndOfProbation() {
+        return endOfProbation;
+    }
+
+    public void setEndOfProbation(LocalDate endOfProbation) {
+        this.endOfProbation = endOfProbation;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ParentUser that = (ParentUser) o;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
---
+++ src/main/java/jd5/ShelterBot/shelterBot/repository/ParentUserRepository.java
@@ -0,0 +1,8 @@
+package jd5.ShelterBot.shelterBot.repository;
+
+import jd5.ShelterBot.shelterBot.model.ParentUser;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ParentUserRepository extends JpaRepository<ParentUser, Long> {
+
+}
