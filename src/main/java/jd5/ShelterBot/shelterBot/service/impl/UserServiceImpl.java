64c562335e5bc212078c0115617fdd1c3796b4c5
Андрей <gosudarev_a@mail.ru>
2022-10-10 17:07:21 +0300

    Исправления в имплементацию UserServiceImpl + удалил дубль  - ParentRepository  + внес 1 метод в ParentUserRepository

--- src/main/java/jd5/ShelterBot/shelterBot/service/impl/UserServiceImpl.java
+++ src/main/java/jd5/ShelterBot/shelterBot/service/impl/UserServiceImpl.java
@@ -4,7 +4,7 @@ import jd5.ShelterBot.shelterBot.model.ParentUser;
 import jd5.ShelterBot.shelterBot.model.ShelterType;
 import jd5.ShelterBot.shelterBot.model.ShelterUser;
 import jd5.ShelterBot.shelterBot.model.UserType;
-import jd5.ShelterBot.shelterBot.repository.ParentRepository;
+import jd5.ShelterBot.shelterBot.repository.ParentUserRepository;
 import jd5.ShelterBot.shelterBot.repository.UserRepository;
 import jd5.ShelterBot.shelterBot.service.UserService;
 import org.springframework.stereotype.Service;
@@ -15,9 +15,9 @@ import java.util.Optional;
 @Service
 public class UserServiceImpl implements UserService {
     private final UserRepository userRepository;
-    private final ParentRepository parentRepository;
+    private final ParentUserRepository parentRepository;
 
-    public UserServiceImpl(UserRepository repository, ParentRepository parentRepository) {
+    public UserServiceImpl(UserRepository repository, ParentUserRepository parentRepository) {
         this.userRepository = repository;
         this.parentRepository = parentRepository;
     }
