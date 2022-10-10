e926e4591489369ff9f392654eb9480861d5961f
AndreyGos1986 <100604436+AndreyGos1986@users.noreply.github.com>
2022-10-10 22:27:08 +0300

    Revert "Feature a gosudarev"

--- src/main/java/jd5/ShelterBot/shelterBot/ShelterBotApplication.java
+++ src/main/java/jd5/ShelterBot/shelterBot/ShelterBotApplication.java
@@ -2,10 +2,7 @@ package jd5.ShelterBot.shelterBot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.PropertySource;
 
-//В папку resources надо добавить свой файл "custom.properties" и в нем прописать данные для базы и токен телеги, добавить его в гит игнор
-@PropertySource(value = "classpath:custom.properties", encoding = "UTF-8")
 @SpringBootApplication
 public class ShelterBotApplication {
 
